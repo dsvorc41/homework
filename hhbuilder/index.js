a348ca80ae4dfcc76d26496eb9c8cdfa1fc1bcf6
dsvorc41 <danilosvorcan@live.com>
2017-04-28 09:40:24 -0700

    Initial commit

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,1 +1,9 @@
+/*
+* Validate data entry (age is required and > 0, relationship is required)
+* Add people to a growing household list
+* Remove a previously added person from the list
+* Display the household list in the HTML as it is modified
+* Serialize the household as JSON upon form submission as a fake trip to the server
+*/
+
 // Your code goes here
